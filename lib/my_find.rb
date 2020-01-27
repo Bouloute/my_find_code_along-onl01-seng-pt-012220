5f7fdcf4ffa7869132f1e83776b865fd8baa4816
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-01-27 15:28:30 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,12 @@
 require 'pry'
 
 def my_find(collection)
-
+  i = 0
+  while i < collection.length
+    thing = yield(collection[i])
+    binding.pry
+    i = i + 1
+  end
 end
+
+collection = (1..100).to_a
